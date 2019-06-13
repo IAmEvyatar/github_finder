adca47c86e5652160c8e807039cff60345cb50fe
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-06-13 15:12:03 +0300

    refactors fc to class based

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { Component } from "react";
 import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className='App'>
-      <h1>hello!</h1>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>hello!</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
