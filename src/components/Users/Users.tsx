f04a3d53b5d09a96e2a29d7dfa93309157726588
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-06-15 22:13:08 +0300

    move state to App component, use axios to get users data from github api and pass down to Users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,32 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component<any, any> {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt"
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett"
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => {
+        {this.props.users.map((user: any) => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
