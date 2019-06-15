c5976a3b6c31a4ecdd4d88850cf2de8b426760de
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-06-15 21:12:55 +0300

    refactor UserItem to functional component from class component

--- src/components/Users/UserItem.tsx
+++ src/components/Users/UserItem.tsx
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 // import PropTypes from "prop-types";
 
-export class UserItem extends Component<any, any> {
-  render() {
-    const { id, login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{
-            width: "60px"
-          }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem: any = (props: any) => {
+  const { id, login, avatar_url, html_url } = props.user;
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{
+          width: "60px"
+        }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
