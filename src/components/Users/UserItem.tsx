c3c66ab2189a9afb7c623db968abb26130857a00
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-07-04 02:04:08 +0300

    add spinner component and show when Users component awaits data

--- src/components/Users/UserItem.tsx
+++ src/components/Users/UserItem.tsx
@@ -14,7 +14,7 @@ const UserItem: any = ({ user: { login, avatar_url, html_url } }: any) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <a href={html_url} target='_blank' className='btn btn-dark btn-sm my-1'>
           More
         </a>
       </div>
