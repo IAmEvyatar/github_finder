7a6cf5f46c28132eadede2fc9eee5d890de0c27c
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-07-07 18:09:50 +0300

    fix url and destructure user info into user component

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ class App extends Component {
   getUser = async (username: string) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}&client_id=${
+      `https://api.github.com/users/${username}?client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
